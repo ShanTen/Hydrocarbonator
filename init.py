683ae24005fdd4abf87f133b1500b211d0c19666
Shan10Code <jojogithub@gmail.com>
2022-03-05 00:59:53 +0530

    updated init

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 import pickle as p
 with open("./cache.bin","wb") as f:
-    empSet = {"CH4":"Methane"}
+    empSet = {"CH4":"Methane"} #Empty dict is uncomfortable so we start with a preset one.
     p.dump(empSet,f)
     f.flush();f.close()
 print("Cache set up.")
